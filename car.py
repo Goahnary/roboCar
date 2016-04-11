0038be6aa22f3942da24049b6021b96323301991
Ty <tyler@faceyspacies.com>
2016-04-11 09:56:58 -0500

    add basic car communication thing

---
+++ car.py
@@ -0,0 +1,54 @@
+import time
+import socket
+import sys
+
+port = 1337
+
+# create our socket, or quit trying
+try:
+	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	ip = socket.gethostbyname( "wkuacm.org" );
+except socket.error:
+	print "failed to create socket :("
+	sys.exit(1)
+except socket.gaierror:
+	print "failed to resolve wkuacm.org"
+	sys.exit(1)
+	
+# connects to our server and asks for input
+clientSocket.connect((ip, port)) # accepts a tuple of the ip and port
+
+print "Enter text to be uppercased"
+
+if(handleConnect(clientSocket)):
+	receiveCommands(clientSocket)
+
+
+
+def processCommand(rawCommand):
+	if(rawCommand.indexOf(" ") == -1):
+		# All commands have a space in them
+		return
+
+receiveCommands(clientSocket):
+	running = True
+	while(running):
+		#clientSocket.send(message + "\r\n") # sends to server, without the \r\n it doesn't work
+		response = clientSocket.recv(1024)  # get response
+		print response.replace("\r\n","")   # remove extra new line characters
+		processCommand(response)            # send the command for processing
+		
+	
+	clientSocket.close()
+
+def handleConnect(clientSocket):
+	clientSocket.send("CONNECT CAR\r\n")    # Tell the server a car is here
+	while(True):
+		
+		response = clientSocket.recv(1024)
+		if(response == "CONTROL DISCONNECTED"):
+			time.sleep(30)
+			clientSocket.send("CONTROL STATUS\r\n")    # Tell the server a car is here
+			continue
+		else:
+			return True
